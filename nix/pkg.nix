7c4f86a7c719fbee211b7677896436b303a6686d
firesquid6 <jdeiss06@gmail.com>
2024-05-30 11:41:39 -0500

    try to add a nixpkg

---
+++ nix/pkg.nix
@@ -0,0 +1,25 @@
+{ lib, buildGoModule, fetchFromGitHub }:
+
+
+buildGoModule rec {
+  pname = "scratch";
+  version = "1.0.0";
+
+  src = fetchFromGittHub {
+    owner = "firesquid6";
+    repo = "scratch";
+    rev = "v${version}";
+    hash = ""
+  };
+
+  meta = with lib; {
+    description = "A simple project manager";
+    homepage = "https://github.com/firesquid6/scratch";
+    license = licenses.gpl3;
+    longDescription = ''
+      Scratch is a simple project manager taht allows you to easily create and manage projects
+      and scratchpads through the command line.
+    '';
+    maintainers = with maintainers; [ firesquid6 ];
+  };
+}
